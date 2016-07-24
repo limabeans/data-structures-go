1242a21de468c9c23cebe150db7088008a62127a
Angel Lim <toomanytypos@gmail.com>
2016-07-24 12:10:57 -0700

    appnd

--- linked-list.go
+++ linked-list.go
@@ -3,7 +3,41 @@ package main
 import "fmt"
 
 
+type node struct {
+	val int
+	next *node
+}
+
+
+
+// walk to end of the linked list and add a new node with x.
+func appnd(curr *node, x int) {
+	// can't append to a nil node
+	if curr == nil {
+		return
+	} else {
+		for (*curr).next != nil {
+			curr = (*curr).next
+		}
+		tmp := node{val: x}
+		(*curr).next = &tmp
+	}
+}
+
+
+// print the linked list!
+func prnt(head *node) {
+	tmp := head
+	for tmp != nil {
+		fmt.Println((*tmp).val)
+		tmp = (*tmp).next
+	}
+}
 
 func main() {
-	fmt.Println("hi there")
+	tmp := node{val: 5}
+	appnd(&tmp, 6)
+	appnd(&tmp, 7)
+	prnt(&tmp)
+	
 }
